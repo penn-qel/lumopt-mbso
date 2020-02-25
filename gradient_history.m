4cfab4d0fbbda0245d5681ada1d9da3574896d8b
Amelia Klein <amyklein37@gmail.com>
2020-02-25 11:19:46 -0500

    Implemented redblue colormap on gradient history plots

--- gradient_history.m
+++ gradient_history.m
@@ -18,6 +18,9 @@ open(v);
 
 height = double(height);
 
+%Create vector to define how gradients are color mapped
+maxgrad = max(max(abs(grad)));
+colorvector = -maxgrad:(2*maxgrad/N):maxgrad;
 
 F(iterations) = struct('cdata',[],'colormap',[]);
 for i = 1:iterations
@@ -28,17 +31,17 @@ for i = 1:iterations
     x = init_pos + offsets;
     widths = params(i,N/2+1:end);
     
-    %Distribute gradients into bins
-    [counts, edges, bin] = histcounts(grad(i,:));
+    %Distribute gradients into bins defined by uniformly spaced edges
+    Y = discretize(widthgrad, colorvector);
+    Z = discretize(offsetgrad, colorvector);
     
-    %Create colormap based on number of bins
-    colorVec = hsv(length(counts));
+    %Create colormap based on number of objects
+    colorVec = redblue(N);
     
-    maxgrad = max(abs(grad(i,:)));
     for j = 1:length(widths)
         centerpos = x(j);
         width = widths(j);
-        rectangle('Position', [centerpos-width/2 0 width height], 'FaceColor', colorVec(bin(j),:), 'LineStyle', 'none');
+        rectangle('Position', [centerpos-width/2 0 width height], 'FaceColor', colorVec(Y(j),:), 'LineStyle', 'none');
     end
     minpos = (min(init_pos) - 1.1*max(max(params)));
     maxpos = (max(init_pos) + 1.1*max(max(params)));
@@ -47,6 +50,10 @@ for i = 1:iterations
     aspect = figwidth/figheight;
     
     text(0, height*1.3, strcat('Iteration #', num2str(i)));
+    
+    colormap(colorVec);
+    caxis([-maxgrad maxgrad])
+    colorbar;
 
     axis([minpos, maxpos, 0, figheight])
     yticks([])

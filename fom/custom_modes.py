63f0c1b2fc440d327768426e3f15a32433e8d7f0
Amelia Klein <amklein@seas.upenn.edu>
2023-06-18 17:00:02 -0400

    Added Gaussian mode

--- fom/custom_modes.py
+++ fom/custom_modes.py
@@ -51,11 +51,11 @@ def AiryDisk(depth, radius, index, x0, y0, z0, norm = np.array([0, 0, -1]), pol_
     return Emodefun, Hmodefun
 
 def AiryDiskVectorized(depth, radius, index, x0, y0, z0, norm = np.array([0, 0, -1]), pol_norm = np.array([1, 0, 0])):
-    #Depth = focal depth of lens
+    '''Depth = focal depth of lens
     #radius = radius of lens
     #index = index of refraction of medium
     #norm: normal vector of propagation direction
-    #pol_norm: Polarization direction of electric field
+    #pol_norm: Polarization direction of electric field'''
     if norm.size != 3:
         raise UserWarning('Normal vector should be a 3x1 vector')
     if np.dot(norm, pol_norm) != 0:
@@ -86,6 +86,90 @@ def AiryDiskVectorized(depth, radius, index, x0, y0, z0, norm = np.array([0, 0, 
         return np.outer(out, Hnorm/Z)
 
     return Emodefun, Hmodefun
+
+def Gaussian(**kwargs):
+    """Returns E(x,y,z,wl) and H(x,y,z,wl) functions for Gaussian Beam. Each set of coordinates
+    is a (N,) array. Assumes beam propagates in z direction and ignores exact z coordinate
+
+    Keyword arguments
+    -----------
+    :kwarg norm:       direction of propagation as numpy array. Default np.array([0,0,1])
+    :kwarg pol_norm:   Polarization direction of E-field as numpy array. Default np.array([1,0,0])
+    :kwarg index:      Index of refraction of medium. Default 1
+    :kwarg z:          z distance from beam waist. Default 0
+    :kwarg theta:      Divergence angle in radians. Default None. Overrides NA if provided
+    :kwarg NA:         Numerical aperture of beam. Default 0.2 
+    :kwarg waist:      Radius of beam waist. Overrides NA and theta if provided. Default None
+    :kwarg x0:         x-coordinate of center of beam. Default 0
+    :kwarg y0:         y-coordinate of center of beam. Default 0
+    :kwarg E0:         Field amplitude at origin. Default 1
+    """
+    #Unpack kwargs
+    n = kwargs.get('index', 1)
+    zprop = kwargs.get('z', 0)
+    NA = kwargs.get("NA", 0.2)
+    theta = kwargs.get("theta")
+    waist = kwargs.get("waist")
+    norm = kwargs.get('norm', np.array([0,0,1]))
+    pol_norm = kwargs.get('pol_norm', np.array([1,0,0]))
+    x0 = kwargs.get("x0", 0)
+    y0 = kwargs.get("y0", 0)
+    E0 = kwargs.get("E0", 1)
+
+    #Calculate divergence angle from NA
+    if theta is None:
+        theta = np.arcsin(NA/n)
+
+    #Handle norm vectors
+    if norm.size != 3:
+        raise UserWarning('Normal vector should be a 3x1 vector')
+    if np.dot(norm, pol_norm) != 0:
+        raise UserWarning('Polarization normal should be orthogonal to propagation normal')
+    norm = norm / np.linalg.norm(norm)
+    pol_norm = pol_norm / np.linalg.norm(pol_norm)
+    Hnorm = np.cross(norm, pol_norm)
+    Z = np.sqrt(sp.constants.mu_0/(np.power(n, 2)*sp.constants.epsilon_0))
+
+    #Take in points as (N,) arrays
+
+    #Calculate scalar field without polarization
+    def scalar(x,y,z,wl):
+        #Beam waist
+        if waist is not None:
+            w0 = waist*np.ones(x.size)
+        else:
+            w0 = wl/(np.pi*np.tan(theta))
+
+        #Rayleigh length
+        zr = np.pi*w0**2*n/wl
+
+        #Beam radius
+        w = w0*np.sqrt(1+(zprop/zr)**2)
+
+        #Calculate r
+        r = np.sqrt((x-x0)**2 + (y-y0)**2)
+
+        #Calculate phase
+        if zprop == 0:
+            phase = np.zeros(x.size)
+        else:
+            k = 2*np.pi*n/wl
+            R = z*(1+(zr/z)**2)
+            phase = k*zprop-np.arctan(zprop/zr)+k*r**2/(2*R)
+
+        #Return field
+        return E0*(w0/w)*np.exp(-r**2/w**2)*np.exp(-1j*phase)
+
+    #Calculate E field
+    def Emodefun(x,y,z,wl):
+        return np.outer(scalar(x,y,z,wl), pol_norm)
+
+    #Calculate H field
+    def Hmodefun(x,y,z,wl):
+        return np.outer(scalar(x,y,z,wl), Hnorm/Z)
+
+    return Emodefun, Hmodefun
+
 
 def PlaneWave(index, norm = np.array([0, 0, 1]), pol_norm = np.array([1, 0, 0])):
     if norm.size != 3:

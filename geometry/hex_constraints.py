70b6f51630c788d94269cddf285c5976a9e521b9
Amelia Klein <amklein@seas.upenn.edu>
2023-12-05 18:11:50 -0500

    First working commit of hexagon metasurface
    Added gradient calculation, plotting, companion constraints object, and miscellaneous helper functions

---
+++ geometry/hex_constraints.py
@@ -0,0 +1,83 @@
+###################################################################
+# Class: constraints.py
+
+# Description: This class generates constraint functions for a 3D
+# metasurface of elliptical pillars allowed to move around within fabrication constraints
+# Author: Amelia Klein
+###################################################################
+
+import numpy as np
+from geometry.constraints import PillarConstraints
+from scipy.optimize import NonlinearConstraint
+import itertools
+from utils.nearest_neighbor_iterator import nearest_neighbor_iterator
+from scipy.sparse import csr_matrix
+from collections import deque
+
+class HexConstraints(PillarConstraints):
+    """
+        :param geo:             Handle to HexagonMetasurface object for optimization geometry
+        :param manual_jac:      Boolean determining whether to provide manual jacobian calculation or to use finite differences
+        :param print_warning:   Boolean determining whether to print when a constraint has a value < 0
+        :param save_list:       Boolean determining whether to generate and save the list of constraints pairs once at start up
+    """
+
+    def __init__(self, geo, manual_jac = True, print_warning = True, save_list = False):
+        '''The constructor for the HexConstraints class'''
+
+        super().__init__(geo, manual_jac = manual_jac, print_warning = print_warning, save_list = save_list)
+
+    def physical_constraint(self, params):
+        '''Returns array of constraints according to pairs of elements in physical units of nm'''
+        offset_x, offset_y, rad = self.geo.get_from_params(params)
+        x = offset_x + self.geo.init_x
+        y = offset_y + self.geo.init_y
+        bound = self.geo.min_feature_size
+        cons = []
+        for pair in self.get_pair_iterator():
+            i, j = pair[0], pair[1]
+            cons.append((np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2) - rad[i] - rad[j] - bound))
+        
+        cons = np.array(cons)
+        if self.print_warning and np.min(cons) < 0:
+            print('Warning: Constraints violated by {} nm'.format(np.min(cons*1e9)))
+        return cons
+
+    def scaled_jacobian(self, params):
+        '''Returns jacobian on same scale as the scaled constraint in sparse array format'''
+        offset_x, offset_y, r = self.geo.get_from_params(params)
+        x = offset_x + self.geo.init_x
+        y = offset_y + self.geo.init_y
+        N = x.size
+        row,col,jac = [np.empty(self.num_constraints*6) for _ in range(3)]
+        loc = 0
+        row_number = 0
+        for pair in self.get_pair_iterator():
+            '''Calculates nonzero Jacobian elements. There should be 8 for each pair'''
+            i, j = pair[0], pair[1]
+            denom = np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
+
+            #Calculate positional components
+            dxi = (x[i]-x[j])/denom
+            dxj = (x[j]-x[i])/denom
+            dyi = (y[i]-y[j])/denom
+            dyj = (y[j]-y[i])/denom
+
+            #Calculate radial components
+            dri = -1
+            drj = -1
+
+            #Assign locations and values to relevant arrays
+            row[loc],col[loc],jac[loc],loc=row_number,i,dxi,loc+1
+            row[loc],col[loc],jac[loc],loc=row_number,j,dxj,loc+1
+            row[loc],col[loc],jac[loc],loc=row_number,N+i,dyi,loc+1
+            row[loc],col[loc],jac[loc],loc=row_number,N+j,dyj,loc+1
+            row[loc],col[loc],jac[loc],loc=row_number,2*N+i,dri,loc+1
+            row[loc],col[loc],jac[loc],loc=row_number,2*N+j,drj,loc+1
+
+            row_number += 1
+
+        jac = csr_matrix((jac, (row,col)), shape=(self.num_constraints,3*N)).toarray()
+        if hasattr(self.geo, "active"):
+            return jac[:,np.concatenate((self.geo.active, self.geo.active, self.geo.active, self.geo.active, self.geo.active))]
+        return jac
--- geometry/hexagon_metasurface.py
+++ geometry/hexagon_metasurface.py
@@ -39,7 +39,7 @@ class HexagonMetasurface(Geometry):
     Optional kwargs
     ---------------
         :kwarg height_precision:Number of points along height of each pillar used to calculate gradient. Default 10
-        :kwarg angle_precision: Number of points along circumference of pillar used to calculate gradient. Default 20
+        :kwarg edge_precision: Number of points along each edge of hexagon used to calculate gradient. Default 4
         :kwarg scaling_factor:  Factor to scale all position and radius parameters. Default 1
         :kwarg limit_nearest_neighbor_cons: Flag to limit constraints to nearest neighbors in initial grid. Default True
         :kwarg make_meshgrid:   Flag to automatically make meshgrid of input x and y points. Default False.
@@ -51,7 +51,7 @@ class HexagonMetasurface(Geometry):
         
         #Unpack kwargs
         height_precision = kwargs.get('height_precision', 10)
-        angle_precision = kwargs.get('angle_precision', 20)
+        edge_precision = kwargs.get('edge_precision', 4)
         scaling_factor = kwargs.get('scaling_factor', 1)
         limit_nearest_neighbor_cons = kwargs.get('limit_nearest_neighbor_cons', True)
         make_meshgrid = kwargs.get('make_meshgrid', False)
@@ -66,10 +66,10 @@ class HexagonMetasurface(Geometry):
         #Option for constructing meshgrid out of x and y arrays automatically
         if make_meshgrid:
             x0, y0 = np.meshgrid(self.init_x, self.init_y, indexing='ij')
-            rx0, ry0 = np.meshgrid(self.r, self.r, indexing = 'ij')
+            r1, r2 = np.meshgrid(self.r, self.r, indexing = 'ij')
             self.init_x = x0.flatten()
             self.init_y = y0.flatten()
-            self.rx = rx0.flatten()
+            self.r = r1.flatten()
 
         self.offset_x = np.zeros(self.init_x.size).flatten()
         self.offset_y = np.zeros(self.init_x.size).flatten()
@@ -78,7 +78,7 @@ class HexagonMetasurface(Geometry):
         self.eps_out = eps_out if isinstance(eps_out, Material) else Material(eps_out)
         self.eps_in = eps_in if isinstance(eps_in, Material) else Material(eps_in)
         self.height_precision = int(height_precision)
-        self.angle_precision = int(angle_precision)
+        self.edge_precision = int(edge_precision)
 
         if self.h <= 0:
             raise UserWarning("pillar height must be positive.")
@@ -107,7 +107,6 @@ class HexagonMetasurface(Geometry):
 
     def add_geo_impl(self, sim, only_update, params, h, z, groupname = 'Pillars'):
         '''Called by add_geo. Implements actual pushing of parameters to Lumerical'''
-        #TODO
 
         offset_x, offset_y, r = self.get_from_params(params)
         points = self.get_vertex_matrix(params)
@@ -120,6 +119,7 @@ class HexagonMetasurface(Geometry):
             sim.fdtd.set('z', 0)
             sim.fdtd.adduserprop('posx', 6, offset_x + self.init_x)
             sim.fdtd.adduserprop('posy', 6, offset_y + self.init_y)
+            sim.fdtd.adduserprop('r', 6, r)
             sim.fdtd.adduserprop('points', 6, points)
             sim.fdtd.adduserprop('height', 0, h)
             sim.fdtd.adduserprop('z0', 0, z)
@@ -138,14 +138,88 @@ class HexagonMetasurface(Geometry):
 
     def update_geometry(self, params, sim = None):
         '''Updates internal values of parameters according to input'''
-        self.offset_x, self.offset_y, self.rx, self.ry, self.phi = self.get_from_params(params)
+        self.offset_x, self.offset_y, self.r = self.get_from_params(params)
         if self.params_debug:
             self.print_current_params()
 
     def calculate_gradients(self, gradient_fields):
         '''Calculates gradient at each wavelength with respect to all parameters'''
  
-        #TODO
+        #Split into six segments, parameterized by pairs of vertices
+        eps_in = self.eps_in.get_eps(gradient_fields.forward_fields.wl)
+        eps_out = self.eps_out.get_eps(gradient_fields.forward_fields.wl)
+        eps_0 = sp.constants.epsilon_0
+        wl = gradient_fields.forward_fields.wl
+
+        z = np.linspace(self.z, self.z + self.h, self.height_precision)
+
+        #parameterize line segments for each hex. NOTE: do we include endpoints??
+        t = np.linspace(0,1,self.edge_precision+1, endpoint=False)
+        t = t[1:]
+
+        hexes = np.arange(self.r.size)
+        segments = np.arange(6)
+
+        #Nx6xedge_precisionxheight_precision matrix of points
+        hexv, segmentv, tv, zv = np.meshgrid(hexes, segments, t, z, indexing='ij')
+
+        x0 = self.offset_x[hexv] + self.init_x[hexv]
+        y0 = self.offset_y[hexv] + self.init_y[hexv]
+        r = self.r[hexv]
+
+        #Calculate (x,y) for all points
+        xv, yv = HexagonMetasurface.get_hex_point(x0, y0, r, segmentv, tv)
+
+        #Get fields
+        Ef, Df = interpolate_fields(xv.flatten(), yv.flatten(), zv.flatten(), gradient_fields.forward_fields)
+        Ea, Da = interpolate_fields(xv.flatten(), yv.flatten(), zv.flatten(), gradient_fields.adjoint_fields)
+
+        fieldshape = (hexes.size, 6, t.size, z.size, wl.size, 3)
+        Ef = Ef.reshape(fieldshape)
+        Df = Df.reshape(fieldshape)
+        Ea = Ea.reshape(fieldshape)
+        Da = Da.reshape(fieldshape)
+
+        #Get normal vectors as (Nx6xt.sizexz.size) matrix
+        midx, midy = HexagonMetasurface.get_hex_point(x0, y0, r, segmentv, 0.5)
+        nx = midx - x0 
+        ny = midy - y0
+
+        #Normalize normal vectors
+        nlength = np.sqrt(np.square(nx) + np.square(ny))
+        nx = (nx/nlength).reshape(hexes.size,6,t.size,z.size,1)
+        ny = (ny/nlength).reshape(hexes.size,6,t.size,z.size,1)
+
+        normal = np.zeros((hexes.size, 6, t.size, z.size, 1, 3))
+        normal[:,:,:,:,:,0] = nx
+        normal[:,:,:,:,:,1] = ny
+
+        #Separate parallel and perpendicular fields
+        def project(a,n):
+            return np.expand_dims(np.sum(a*n, axis=-1), axis = 5) * n
+
+        Dfperp = project(Df, normal)
+        Daperp = project(Da, normal)
+        Efpar = Ef - project(Ef, normal)
+        Eapar = Ea - project(Ea, normal)
+
+        #Factors to multiply for each derivative (dot product of normal vector with perturbation vector)
+        d_dx = normal[:,:,:,:,:,0]
+        d_dy = normal[:,:,:,:,:,1]
+        d_dr = 1
+
+        #Adjoint integrand
+        integrand = 2*np.real(eps_0*(eps_in-eps_out)*np.sum(Efpar*Eapar, axis=-1)+ (1/eps_out - 1/eps_in)/eps_0*np.sum(Dfperp*Daperp, axis=-1))
+        #Multiply by arc length of segment (r)
+        curve_integrand = integrand*np.expand_dims(r, axis=4)
+
+        #Integrate over z and t, sum over segments
+        deriv_x = np.sum(np.trapz(np.trapz(d_dx*curve_integrand, z, axis=3),t,axis=2),axis=1)
+        deriv_y = np.sum(np.trapz(np.trapz(d_dy*curve_integrand, z, axis=3),t,axis=2),axis=1)
+        deriv_r = np.sum(np.trapz(np.trapz(d_dr*curve_integrand, z, axis=3),t,axis=2),axis=1)
+
+        return np.concatenate((deriv_x, deriv_y, deriv_r))/self.scaling_factor
+
 
     def calculate_gradients_on_cad(self, sim, forward_fields, adjoint_fields, wl_scaling_factor):
         '''Semi hack to reduce memory usage of gradient calculation. Actual calculation of gradients still done in Python
@@ -201,7 +275,19 @@ class HexagonMetasurface(Geometry):
 
     def plot(self, ax):
         '''Plots current geometry'''
-        print('Plotting function not currently supported')
+        x = (self.offset_x + self.init_x)*1e6
+        y = (self.offset_y + self.init_y)*1e6
+        r = self.r.copy()*1e6
+        maxr = np.amax(r)
+        ax.clear()
+        for i, xval in enumerate(x):
+            hexagon = patches.RegularPolygon((xval, y[i]), 6, radius=r[i], orientation = np.pi/2, facecolor='black')
+            ax.add_patch(hexagon)
+        ax.set_title('Geometry')
+        ax.set_xlim(min(x) - maxr, max(x) + maxr)
+        ax.set_ylim(min(y) - maxr, max(y) + maxr)
+        ax.set_xlabel('x (um)')
+        ax.set_ylabel('y (um)')
         return True
 
     def calculate_bounds(self):
@@ -218,8 +304,8 @@ class HexagonMetasurface(Geometry):
                 'for(i=1:length(posx)) {\n'
                     'addpoly;\n'
                     'set("name", "pillar_"+num2str(i));\n'
-                    'set("x", posx(i));\n'
-                    'set("y", posy(i));\n'
+                    'set("x", 0);\n'
+                    'set("y", 0);\n'
                     'set("z min", z0);\n'
                     'set("z max", z0+height);\n'
                     'vertices = reshape(points(i,:,:), [6,2]);\n'
@@ -280,15 +366,56 @@ class HexagonMetasurface(Geometry):
         return x0 + r*np.cos(i*angle), y0 + r*np.sin(i*angle)
 
     @staticmethod
+    def get_hex_point(x0, y0, r, i, t):
+        '''Returns (x,y) on segment between i and i+1 vertex parameterized by 0<t<1'''
+        
+        #Get endpoints
+        x1, y1 = HexagonMetasurface.get_vertex(x0,y0,r,i)
+        x2, y2 = HexagonMetasurface.get_vertex(x0,y0,r,i+1)
+
+        #Parametric equation of line segment
+        return x1 + (x2 - x1)*t, y1 + (y2 - y1)*t
+
+    @staticmethod
     def get_params_from_existing_simulation(filename, get_wavelengths = False):
         '''Opens an existing .fsp file that was used in an optimization and retrieves geometry 
         Returns parameters as a dict'''
 
-        #TODO
+        #Open simulation file
+        sim = Simulation('./', use_var_fdtd = False, hide_fdtd_cad = True)
+        sim.load(filename)
+
+        #Create dict to store parameters
+        params = dict()
+
+        #Retrieve parameters
+        sim.fdtd.select('Pillars')
+        params['posx'] = sim.fdtd.get('posx').flatten()
+        params['posy'] = sim.fdtd.get('posy').flatten()
+        params['r'] = sim.fdtd.get('r').flatten()
+        params['z'] = sim.fdtd.get('z0')
+        params['h'] = sim.fdtd.get('height')
+
+        if get_wavelengths:
+            f = sim.fdtd.getglobalmonitor('custom frequency samples').flatten()
+            params['wl'] = scipy.constants.speed_of_light/f
+
+        sim.fdtd.close()
+
+        return params
 
     @staticmethod
     def create_from_existing_simulation(filename, get_wavelengths = False):
         '''Creates a geometry object based on an existing structure saved in a .fsp sim file.
         For use with analysis only, as will put dummy parameters for inputs related to optimization'''
 
-        #TODO
+        params = HexagonMetasurface.get_params_from_existing_simulation(filename, get_wavelengths)
+
+        geom = HexagonMetasurface(posx = params['posx'], posy = params['posy'], r = params['r'], 
+            min_feature_size = 0, z = params['z'], h = params['h'], eps_in = 1, eps_out = 2.4**2, 
+            scaling_factor = 1, limit_nearest_neighbor_cons = False)
+
+        if get_wavelengths:
+            return geom, params['wl']
+        else:
+            return geom
